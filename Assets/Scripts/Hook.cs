36a90a38308c6c9ef44cdfb68aea796295c5d928
OmerKilicc <omeerfkiliic@gmail.com>
2021-08-18 17:55:16 +0300

    Initial commit

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Hook : MonoBehaviour
+{
+    public Transform hookedTransform;
+
+    private Camera mainCamera;
+    private CircleCollider2D _circleCollider2D;
+
+    private int length, strength, fishCount;
+
+    private bool canMove = true;
+    private Tweener cameraTween;
+    
+    //Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        _circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove && Input.GetMouseButton(0))
+        {
+            Vector3 vector = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = transform.position;
+            position.x = vector.x;
+            transform.position = position;
+
+        }
+    }
+
+    public void StartFishing()
+    {
+        //TODO Idle Manager
+        length = -50;
+        strength = 3;
+        fishCount = 0;
+
+        float time = (-length) * 0.1f;
+
+        //Transform camera position to given y value
+        cameraTween = mainCamera.transform.DOMoveY(length, 1 + time * 0.25f, false).OnUpdate(delegate 
+        {
+            // Code for making hook dive while going to given y position with camera
+            if (mainCamera.transform.position.y <= -11)
+                transform.SetParent(mainCamera.transform);
+
+        }).OnComplete(delegate
+        {
+            // Code for making hook come back after it reaches its destination meanwhile catching fishes on the road
+            _circleCollider2D.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time * 5, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -25f)
+                {
+                    StopFishing();
+                }
+            });
+        });
+
+        //Screen(GAME)
+        _circleCollider2D.enabled = false;
+        canMove = true;
+        //clear
+    }
+
+    void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y >= -11)
+            {
+                // To not pull the hook to surface
+                transform.SetParent(null);
+                transform.position = new Vector2(transform.position.x, -6);
+            }
+        }).OnComplete(delegate
+        {
+            transform.position = Vector2.down * 6;
+            _circleCollider2D.enabled = true;
+            int num = 0;
+            //Clearing out the hook from the fishes
+            //Idle Manager Totalgain = num;
+            //Screenmanager End Screen
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TheFishMasterClone
+ I made my version of the game named Fish Master for educational purposes
